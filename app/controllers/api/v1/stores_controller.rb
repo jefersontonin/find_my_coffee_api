a572a48d3622e5967777d666173f14848e928add
Jeferson Tonin <jefersontonin77@gmail.com>
2020-10-23 18:19:55 -0300

    added stores controller, routes and show view using jbuilder

---
+++ app/controllers/api/v1/stores_controller.rb
@@ -0,0 +1,16 @@
+class Api::V1::StoresController < ApplicationController
+  before_action :set_store, only: [:show]
+
+  def show
+  end
+
+  def index
+
+  end
+
+  private
+
+  def set_store
+    @store = Store.find_by!(google_place_id: params[:id])
+  end
+end
